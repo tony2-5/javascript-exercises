7f43bea451484e5a49df39fe9fea1cebd8a6a9f9
tony2-5 <antdvory@gmail.com>
2022-07-31 15:10:38 -0400

    Completed exersize 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    cleanString = string.replace(/\W/g, '').toLowerCase();
+    let strRev = cleanString.split('').reverse().join('');
+    if(strRev === cleanString) return true;
+    else return false;
 };
 
 // Do not edit below this line
