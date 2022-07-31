e7109610db0ae2f21f3530d099c69f432d223f6b
tony2-5 <antdvory@gmail.com>
2022-07-31 14:34:03 -0400

    Completed exersize 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(...args) {
+  const sum = args.reduce((total, value) => {return total + value
+  });
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	const sum = args.reduce((total, value) => {return total - value
+  });
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  if(args.length === 0) return 0;
+  else {
+    const sum = args.reduce((total, value) => {return total + value
+    });
+    return sum;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  const sum = args.reduce((total, value) => {return total * value
+  });
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num1, power) {
+	return num1**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	if(num1 === 0) return 1;
+  return num1 * factorial(num1 - 1);
 };
 
 // Do not edit below this line
