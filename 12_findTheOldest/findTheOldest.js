da695c4009c8b617c9d33999d3970b71809cc59e
tony2-5 <antdvory@gmail.com>
2022-08-01 18:52:10 -0400

    Completed exersize 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+   ageSortedArr = array.sort((a, b) => {
+        if(a.yearOfDeath == undefined) a['yearOfDeath'] = 2022;
+        if(b.yearOfDeath == undefined) b['yearOfDeath'] = 2022;
+        const firstAge = a.yearOfDeath - a.yearOfBirth;
+        const secondAge = b.yearOfDeath - b.yearOfBirth;
+        return firstAge > secondAge ? -1:1;
+   });
+   return ageSortedArr[0];
 };
 
 // Do not edit below this line
