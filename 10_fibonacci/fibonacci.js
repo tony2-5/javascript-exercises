21f56e25d40f0d360f7a9abc0dceebf877e20a98
tony2-5 <antdvory@gmail.com>
2022-08-01 08:16:11 -0400

    Completed exersize 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0) return "OOPS";
+    let num1 = 0, num2 = 1, holder = 0;
+    for(let i = 0; i<num-1; i++) {
+        holder = num1 + num2;
+        num1 = num2;
+        num2 = holder;
+    }
+    return num2;
 };
 
 // Do not edit below this line
